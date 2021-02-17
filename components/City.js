46e14694644eb67949b7777af119e74abcdd0040
rokko <rocco.caricola89@gmail.com>
2021-02-17 23:15:14 +0100

    aggiunta geolocalizzazione e permessi

--- components/City.js
+++ components/City.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Text, View } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input } from 'react-native-elements';
+import * as Location from 'expo-location';
 
 export default function City() {
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+    const [citta, setCitta] = useState('')
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        })();
+    }, []);
+
+    let text = 'Waiting..';
+    if (errorMsg) {
+        text = errorMsg;
+    } else if (location) {
+        
+        text = JSON.stringify(location);
+    }
+
+    console.log(text)
+
 
     return (
-        
-            <Text>Ok</Text>
-      
+
+        <Input
+            placeholder='Citta'
+            leftIcon={{ type: 'font-awesome', name: 'map-marker' }}
+            onChange={(test) => { setCitta(test) }}
+        />
+
     )
 }
