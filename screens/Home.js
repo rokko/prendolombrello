d42bab12c5602c1be1ed42a623cd18fdb6a4246e
rokko <rocco.caricola89@gmail.com>
2021-02-19 21:36:43 +0100

    create new component for research city

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import {Text, View} from 'react-native'
+import { Text, View } from 'react-native'
 import City from '../components/City'
 import CarouselCard from '../components/CarouselCard'
-export default function Home(){
+import SearchCitta from '../components/SearchCitta'
+export default function Home() {
     return (
         <View>
-        <City/>
-        <CarouselCard/>
-        
+            <City />
+            <CarouselCard />
+            <SearchCitta />
+
         </View>
     )
 }
